f3c23c9d8e0443212af6f1cf346510aa1a7f21d0
junu0810 <junu0810@hanmail.net>
2023-01-23 01:27:40 +0900

    make Chat by ws

--- clone_zoom/src/public/js/app.js
+++ clone_zoom/src/public/js/app.js
@@ -1,18 +1,43 @@
 //front_End Page
 const socket = new WebSocket(`ws://${window.location.host}`)
 
+const messageList = document.querySelector("ul")
+const nicknameForm = document.querySelector("#nick")
+const messageForm = document.querySelector("#message")
+
+function makeMessage(type , payload) {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
 socket.addEventListener("open" , () => {
     console.log("Connected to Server 🔔")
 })
 
 socket.addEventListener("message" , (message) => {
-    console.log(`New message: ${message.data}`)
+    console.log(`New message: ${message.data.toString()}`)
+    const li = document.createElement("li");
+    li.innerHTML = message.data.toString();
+    messageList.append(li);
 })
 
 socket.addEventListener("close" , () => {
     console.log("Close the WebSite 🔕")
 })
 
-setTimeout(() => {
-    socket.send("hello from the Browser!")
-}, 1000)
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    console.log(input.value);
+    input.value = ""; 
+}
+
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("nickname" , input.value));
+}
+
+messageForm.addEventListener("submit" , handleSubmit);
+nicknameForm.addEventListener("submit" , handleNickSubmit)
--- clone_zoom/src/server.js
+++ clone_zoom/src/server.js
@@ -6,28 +6,44 @@ const app = express();
 
 
 app.set('view engine', 'pug');
-app.set("views" , __dirname +  "/views");
+app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
-const handlListen = () => {console.log('Listening on ws://127.0.0.1:8080')}
+const handlListen = () => { console.log('Listening on ws://127.0.0.1:8080') }
+
+// 연결된 사용자 목록 저장
+const sockets = [];
 
 // http Server
 const server = http.createServer(app);
 // webSocket Server(+ http Server)
-const wss = new WebSocket.Server({ server }); 
+const wss = new WebSocket.Server({ server });
 
 function handleConnection(socket) {
+    sockets.push(socket)
+    socket["nickname"] = "Aon";
     console.log("Connected to Browser 🔔")
     // Browser에서 연결을 끊을경우 출력되는 메세지 
     socket.on("close", () => console.log("Close the Server 🔕"))
     // Browser에서 보낸 메세지를 Listen함
-    socket.on("message", (message) => {
-        console.log(message.toString('utf8'))
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        console.log(message)
+        switch (message.type) {
+            case "new_message":
+                // 저장된 사용자 목록 전체에게 메세지를 보낸다
+                sockets.forEach((el) => {
+                    el.send(`${socket.nickname} : ${message.payload}`);
+                })
+                break
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break
+        }
     });
-    socket.send("hello!!!")
 }
 
-wss.on("connection" , handleConnection )
+wss.on("connection", handleConnection)
 
 server.listen(8080, handlListen);
