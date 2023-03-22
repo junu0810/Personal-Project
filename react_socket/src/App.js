3f1c86c264b465593a66af0e1c61f38ada0441de
junu0810 <junu0810@hanmail.net>
2023-03-22 15:54:11 +0900

    delete file

--- react_socket/src/App.js
+++ react_socket/src/App.js
@@ -1,89 +1,118 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Media from './Media';
+import * as StompJs from "@stomp/stompjs";
+import * as SockJS from "sockjs-client";
 
 function App() {
 
-  const [ chat , setChat ] = useState("");
-  const [ connected , setConnected ] = useState(false);
+  const [chat, setChat] = useState("");
+  const [connected, setConnected] = useState(false);
   const [items, setItems] = useState([]);
-  const [sendMsg, setSendMsg] = useState(false);
+  const [nickName, setNickName] = useState("")
+  const [checkName, setCheck] = useState(false);
 
-  const socketURL = "ws://localhost:8080/socket";
   let ws = useRef(null);
-  
-  useEffect(() => {
-    if(!ws.current){
-      ws.current = new WebSocket(socketURL);
-      ws.current.onopen = () => {
-        console.log(WebSocket.prototype);
-        setConnected(true);
-      ws.current.onclose = (error) => {
-        console.log("disconnect from " + socketURL);
-        console.log(error);
-      };
-      ws.current.onerror = (error) => {
-        console.log("connection error " + socketURL);
-        console.log(error);
-      };
-      ws.current.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        console.log(chat);
-        setItems((prevItems) => [...prevItems, data]);
-      };
-    };
+  let userName = useRef("");
 
-    }
 
-  },[]);
+  // useEffect(() => {
+  //   if (!ws.current) {
+  //     ws.current = new SockJs(socketURL);
+  //     ws.current.onopen = () => {
+  //       console.log(WebSocket.prototype);
+  //       setConnected(true);
+  //       ws.current.onclose = (error) => {
+  //         console.log("disconnect from " + socketURL);
+  //         console.log(error);
+  //       };
+  //       ws.current.onerror = (error) => {
+  //         console.log("connection error " + socketURL);
+  //         console.log(error);
+  //       };
+  //       ws.current.onmessage = (event) => {
+  //         const data = JSON.parse(event.data);
+  //         console.log(chat);
+  //         setItems((prevItems) => [...prevItems, data]);
+  //       };
+  //     };
 
-  // 소켓이 연결되었을 시에 send 메소드
-  useEffect(() => {
-    if (connected) {
-      ws.current.send(
-        JSON.stringify({
-          message: "연결 완료",
-        })
-      );
+  //   }
+  // }, [chat, checkName]);
 
-      setSendMsg(true);
-    }
-  }, [connected]);
+  const connect = () => {
+    ws.current = new StompJs.Client({
+      brokerURL: 'ws://localhost:8080/ws',
+      onConnect: () => {
+        console.log('success');
+      },
+    });
+    ws.current.activate();
+  };  
 
-  const sendData = (event) => {
-    console.log(chat)
-    event.preventDefault()
-    if(connected){
-      ws.current.send(
-        JSON.stringify({
-        message: chat
-      }))
+  useEffect(()=>{
+    connect();
+  },[])
 
-      setChat("")
-    }
-  } 
 
-  const inputChange = (event) => {
-    const {target : { value }} = event;
-    setChat(value)
-    // console.log(chat)
+const sendData = (event) => {
+  event.preventDefault()
+  if (connected) {
+    let message = {}
+    message[nickName] = chat
+    ws.current.send(JSON.stringify(message))
+    setChat("")
   }
+}
 
-  return (
-    <div>
-      {connected ? 
-        <div>연결이 완료되었습니다.</div> :
-        <div>연결 대기중입니다.</div>}
-    <form> 
-      <input onChange={inputChange} value={chat}/>
-      <button onClick={sendData}>전송</button>
-    </form>
-    <div>
-        {items.map((item , ind) => {
-          return <div key={ind}>{JSON.stringify(item)}</div>;
-        })}
-    </div>
-    </div>
-  );
+const inputChange = (event) => {
+  const { target: { value } } = event;
+  setChat(value);
+}
+
+const changeNickName = (event) => {
+  const { target: { value } } = event;
+  setNickName(value);
+}
+
+const inputNickname = (event) => {
+  event.preventDefault()
+  userName.current = nickName;
+  setCheck(true)
+}
+
+return (
+  <div>
+    {connected ?
+      <div>
+        <div>연결이 완료되었습니다.</div>
+        {
+          checkName === false
+            ?
+            <form onSubmit={inputNickname}>
+              <input onChange={changeNickName} placeholder="닉네임을 설정하세요" />
+              <button type="submit">닉네임 설정</button>
+            </form>
+            :
+            <div>
+              <p>{`사용중인 닉네임 : ${nickName}`}</p>
+              <form onSubmit={sendData}>
+                <input onChange={inputChange} value={chat} />
+                <button type="submit">전송</button>
+              </form>
+            </div>
+        }
+        <div>
+          {items.map((item, ind) => {
+            return <p key={ind}>{JSON.stringify(item)}</p>;
+          })}
+        </div>
+        <Media />
+      </div>
+      :
+      <div>연결 대기중입니다.</div>}
+  </div>
+);
 }
 
 export default App;
---
+++ react_socket/src/Media.js
@@ -0,0 +1,14 @@
+
+
+function Media(){
+    
+
+    return(
+        <div>
+            화상회의 화면
+        </div>
+    )
+}
+
+
+export default Media;
